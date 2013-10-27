ae08b59a2e61ade1458a1334e35aa9e9c835b3c3
Magne Davidsen <magne.davidsen@gmail.com>
2013-10-27 08:46:03 +0100

    trying to use yo heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,6 +270,23 @@ module.exports = function (grunt) {
         src: '{,*/}*.css'
       }
     },
+    copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    },
     concurrent: {
       server: [
         'coffee:dist',
